25886ea05e492f9f6133f5325ed49cf96925c8f6
Merrouche Aimen Rami <merrouche.aimenrami@esst-sup.com>
2024-05-16 12:12:57 +0100

    azer

--- adminAccount.php
+++ adminAccount.php
@@ -1,11 +1,76 @@
-<div class="section">
-    <h1>User Profile and Settings</h1>
-    <div>
-        <button class="action-button">Edit Profile</button>
-        <button class="action-button">Change Password</button>
-    </div>
-    <div>
-        <button class="action-button">Dashboard Preferences</button>
-        <button class="action-button">Theme Selection</button>
-    </div>
+<?php
+if (file_exists('dblink.php')) 
+{
+	require 'dblink.php';
+}
+else {
+	die("File not found");
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<div class="navbar">
+    <div class="navbar-brand"><a href="adminHomePage.php">Admin Dashboard</a></div>
+    <ul class="navbar-menu">
+        <li class="navbar-item"><a href="adminHomePage.php" class="nav-link">Dashboard</a></li>
+        <li class="navbar-item"><a href="vehiclesList.php" class="nav-link">Vehicles</a></li>
+        <li class="navbar-item"><a href="driversList.php" class="nav-link">Drivers</a></li>
+        <li class="navbar-item"><a href="maintenanceList.php" class="nav-link">Maintenances</a></li>
+        <li class="navbar-item"><a href="missionList.php" class="nav-link">Missions</a></li>
+        <li class="navbar-item"><a href="#" class="nav-link">Notifications</a></li>
+        
+        <li class="navbar-item"><a href="#" class="nav-link">Reports</a>
+        <ul class="dropdown">
+            <li><a href="driverReportList.php">Driver Reports</a></li>
+            <li><a href="adminReportList.php">Admin Reports</a></li>
+        </ul>
+        </li>
+        
+        <li class="navbar-item"><a href="#" class="nav-link">Expenses Up-to-date</a>
+            <ul class="dropdown">
+                <li><a href="driverExpensesList.php">Driver Expenses</a></li>
+                <li><a href="maintenanceExpensesList.php">Maintenance Expenses</a></li>
+                <li><a href="missionExpensesList.php">Missions Expenses</a></li>
+                <li><a href="vehicleExpensesList.php">Vehicle Expenses</a></li>
+                <li><a href="penaltiesExpensestList.php">Penalties and Fines</a></li>
+            </ul>
+        </li>
+        <li class="navbar-item"><a href="statisticPage.php" class="nav-link">Statistics</a></li>
+        <li class="navbar-item"><a href="#" class="nav-link">Account</a></li>
+    </ul>
 </div>
+
+<div class="container">
+
+<?php
+
+$sql = "SELECT username FROM admin WHERE admin_id = ?";
+$stmt = mysqli_prepare($link, $sql);
+$stmt->bind_param('i', 1);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+$stmt->close();
+?>
+
+    <div class="section">
+        <?php echo "<h1>Welcome, " . $row['username'] . "!</h1>";?>
+    </div>
+    <div class="section">
+        <h2>Logout : </h2>
+        <input type="button" value="LogOut" onclick="window.location.href='index.html'">
+
+
+</div>
+</body>
+</html>
+<?php mysqli_close($link); ?>
