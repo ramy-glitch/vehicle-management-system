f7223bd4e602aaffc66ec55ded0c8b0c2677321d
Merrouche Aimen Rami <merrouche.aimenrami@esst-sup.com>
2024-05-18 12:44:27 +0100

    Update adminHomePage.php

--- adminHomePage.php
+++ adminHomePage.php
@@ -78,7 +78,7 @@ else {
     </div>
 
     <div class="section">
-        <h1>Quick Access</h1>
+        <h2 style="color: blue;">Quick Access</h2>
         <div>
             <button class="action-button" onclick="window.location.href='vehicleForm.php'">Add New Vehicle</button>
             <button class="action-button" onclick="window.location.href='driverForm.php'">Add New Driver</button>
@@ -89,7 +89,7 @@ else {
     </div>
 
     <div class="section">
-        <h1> Today's Scheduled Missions:</h1>
+        <h2 style="color: blue;"> Today's Scheduled Missions:</h2>
         <div>
             <table>
                 <tr>
@@ -112,7 +112,7 @@ else {
                         }
                     }
                     
-                    $sql = "SELECT mission_id, purpose, driver_name, vehicle_model, start_date_time, end_date_time, end_location, mission_status FROM mission JOIN driver ON mission.driver_id = driver.driver_id JOIN vehicle ON mission.vehicle_id = vehicle.vehicle_id WHERE mission_status = 'scheduled' AND DATE(start_date_time) = CURDATE()";
+                    $sql = "SELECT mission_id, purpose, driver_name, vehicle_license_plate, start_date_time, end_date_time, end_location, mission_status FROM mission JOIN driver ON mission.driver_id = driver.driver_id JOIN vehicle ON mission.vehicle_id = vehicle.vehicle_id WHERE mission_status = 'scheduled' AND DATE(start_date_time) = CURDATE()";
                     $result = mysqli_query($link, $sql);
 
                     if (mysqli_num_rows($result) > 0) {
@@ -123,7 +123,7 @@ else {
 
                             echo "<td>" . $purpose . "</td>";
                             echo "<td>" . $row['driver_name'] . "</td>";
-                            echo "<td>" . $row['vehicle_model'] . "</td>";
+                            echo "<td>" . $row['vehicle_license_plate'] . "</td>";
                             echo "<td>" . $row['start_date_time'] . "</td>";
                             echo "<td>" . $row['end_date_time'] . "</td>";
                             echo "<td>" . $row['end_location'] . "</td>";
@@ -137,6 +137,120 @@ else {
             </table>
         </div>
 
+    </div>
+
+    <div class="section">
+        <h2 style="color: blue;">Recent Missions:</h2>
+        <div>
+            <table>
+                <tr>
+                    <th>Mission purpose</th>
+                    <th>Driver</th>
+                    <th>Vehicle</th>
+                    <th>Start Time</th>
+                    <th>End Time</th>
+                    <th>Destination</th>
+                    <th>Status</th>
+                </tr>
+                <?php
+                    $sql = "SELECT mission_id, purpose, driver_name, vehicle_license_plate, start_date_time, end_date_time, end_location, mission_status FROM mission JOIN driver ON mission.driver_id = driver.driver_id JOIN vehicle ON mission.vehicle_id = vehicle.vehicle_id WHERE mission_status = 'in_progress' ORDER BY start_date_time DESC LIMIT 5";
+                    $result = mysqli_query($link, $sql);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            $purpose = truncateString($row['purpose'], 30) ;
+                            echo "<td>" . $purpose . "</td>";
+                            echo "<td>" . $row['driver_name'] . "</td>";
+                            echo "<td>" . $row['vehicle_license_plate'] . "</td>";
+                            echo "<td>" . $row['start_date_time'] . "</td>";
+                            echo "<td>" . $row['end_date_time'] . "</td>";
+                            echo "<td>" . $row['end_location'] . "</td>";
+                            echo "<td>" . $row['mission_status'] . "</td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='7'>No recent missions.</td></tr>";
+                    }
+                ?>
+            </table>
+        </div>
+
+    </div>
+
+    <div class="section">
+        <h2 style="color: blue;">Today's scheduled Maintenances:</h2>
+        <div>
+            <table>
+                <tr>
+                    <th>Maintenance Type</th>
+                    <th>Vehicle</th>
+                    <th>Start Date</th>
+                    <th>Cost</th>
+                    <th>Status</th>
+                </tr>
+                <?php
+                // The CURDATE() function in MySQL returns the current date in the format ‘YYYY-MM-DD’.
+
+                    $sql = "SELECT maintenance_type, vehicle_license_plate, maintenance_date, cost, maintenance_status FROM vehicle_maintenance JOIN vehicle ON vehicle_maintenance.vehicle_id = vehicle.vehicle_id where (maintenance_status = 'scheduled' AND maintenance_date = CURDATE())";
+                    $result = mysqli_query($link, $sql);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            $maintenance_type = truncateString($row['maintenance_type'], 20) ;
+                            echo "<td>" . $maintenance_type . "</td>";
+                            echo "<td>" . $row['vehicle_license_plate'] . "</td>";
+                            echo "<td>" . $row['maintenance_date'] . "</td>";
+                            echo "<td>" . $row['cost'] . "</td>";
+                            echo "<td>" . $row['maintenance_status'] . "</td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='5'>No recent maintenance.</td></tr>";
+                    }
+                ?>
+            </table>
+        </div>
+
+    </div>
+
+
+    <div class="section">
+        <h2 style="color: blue;">Today's Recent Penalties:</h2>
+        <div>
+            <table>
+                <tr>
+                    <th>Driver</th>
+                    <th>Driver License</th>
+                    <th>Penalty Date</th>
+                    <th>Amount</th>
+                    <th>Reason</th>
+                </tr>
+                <?php
+                    $sql = "SELECT driver_name,driver_license_number, penality_date, penality_cost , penality_type FROM penality_expense JOIN driver ON penality_expense.driver_id = driver.driver_id where penality_date = CURDATE()";
+                    $result = mysqli_query($link, $sql);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>" . $row['driver_name'] . "</td>";
+                            echo "<td>" . $row['driver_license_number'] . "</td>";
+                            echo "<td>" . $row['penality_date'] . "</td>";
+                            echo "<td>" . $row['penality_cost'] . "</td>";
+                            $reason = truncateString($row['penality_type'], 20) ;
+                            echo "<td>" . $reason . "</td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='5'>No recent penalties.</td></tr>";
+                    }
+                ?>
+            </table>
+        </div>
+
+    
+    </div>
 
 
     <div class="section">
