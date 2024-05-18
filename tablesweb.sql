f335025900070afcf85f43dad1f01fa0eb973105
Merrouche Aimen Rami <merrouche.aimenrami@esst-sup.com>
2024-05-18 07:03:44 +0100

    Update tablesweb.sql

--- tablesweb.sql
+++ tablesweb.sql
@@ -24,13 +24,13 @@ create table vehicle (
 create table vehicle_maintenance (
     maintenance_id int not null auto_increment,
     vehicle_id int not null,
-    maintenance_date date not null,
+    maintenance_date varchar(10) not null,
     maintenance_type varchar(50) not null,
     maintenance_description text not null,
     workshop_name varchar(50) not null,
     workshop_phone varchar(50) not null,
     cost float not null,
-    next_maintenance_date date not null,
+    next_maintenance_date varchar(10) not null,
     maintenance_status varchar(30) not null,
 
     constraint pk_maintenance_id primary key (maintenance_id),
@@ -43,12 +43,12 @@ create table vehicle_maintenance (
 CREATE TABLE driver (
     driver_id int NOT NULL AUTO_INCREMENT,
     driver_name varchar(50) NOT NULL,
-    driver_birthdate date NOT NULL,
+    driver_birthdate varchar(10) NOT NULL,
     driver_phone varchar(50) NOT NULL,
     driver_address varchar(255) NOT NULL,
     username varchar(50) NOT NULL,
     pwd varchar(255) NOT NULL,
-    employment_date date NOT NULL,
+    employment_date varchar(10) NOT NULL,
     monthly_salary float NOT NULL,
     driver_history text NOT NULL,
     driver_status varchar(50) NOT NULL,
@@ -89,7 +89,7 @@ create table mission (
 create table penality_expense (
     penality_id int not null auto_increment,
     driver_id int not null,
-    penality_date date not null,
+    penality_date varchar(10) not null,
     penality_type varchar(255) not null,
     penality_cost float not null,
 
@@ -99,7 +99,7 @@ create table penality_expense (
 
 create table admin_report (
     report_id int not null auto_increment,
-    report_date date not null,
+    report_date varchar(10) not null,
     report_issue varchar(255) not null,
     report_description text not null,
 
@@ -110,7 +110,7 @@ create table admin_report (
 create table driver_report (
     report_id int not null auto_increment,
     driver_id int not null,
-    report_date date not null,
+    report_date varchar(10) not null,
     report_issue varchar(255) not null,
     report_description varchar(255) not null,
 
