b7d4477a0e0897bad11b4d59dc222acf8229d09a
Merrouche Aimen Rami <merrouche.aimenrami@esst-sup.com>
2024-05-21 13:33:10 +0100

    Update missionform.php

--- missionform.php
+++ missionform.php
@@ -173,7 +173,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <option value="none" <?php if ($vehicle_assignment === 'none') echo "selected"; ?>>None</option>
                     <?php
                     // SQL query to retrieve vehicle data
-                    $sql = "SELECT vehicle_id, vehicle_license_plate, vehicle_type, vehicle_model FROM vehicle WHERE vehicle_status = 'out_of_service'";
+                    $sql = "SELECT vehicle_id, vehicle_license_plate, vehicle_type, vehicle_model,vehicle_location FROM vehicle WHERE vehicle_status = 'out_of_service'";
                     $result = mysqli_query($link, $sql);
 
                     // Check if any rows are returned
@@ -184,12 +184,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             $vehicleType = htmlspecialchars($row["vehicle_type"]);
                             $vehicleModel = htmlspecialchars($row["vehicle_model"]);
                             $vehicle_pln = htmlspecialchars($row["vehicle_license_plate"]);
+                            $vehicle_location = htmlspecialchars($row["vehicle_location"]);
 
                             // Determine if this option should be selected
                             $selected = ($vehicle_assignment == $vehicleId) ? "selected" : "";
 
                             // Output the option with the appropriate value and selected attribute
-                            echo '<option value="' . $vehicleId . '" ' . $selected . '>' . $vehicleType . ' ' . $vehicleModel . ' ' . $vehicle_pln . '</option>';
+                            echo '<option value="' . $vehicleId . '" ' . $selected . '>' . $vehicleType . ' ' . $vehicleModel . ' ' . $vehicle_pln . ' ' . $vehicle_location . '</option>';
                         }
                     }
                     ?>
